113f13065738e3052a7b8bb2feb74faf3bad22a3
unknown <Johan@.(none)>
2010-03-20 17:35:54 +0100

    now calculating neighboring fields

--- goGame/src/test/java/GoGameTest.java
+++ goGame/src/test/java/GoGameTest.java
@@ -1,20 +1,16 @@
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
+import static java.util.Arrays.asList;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-import javax.swing.text.StyledEditorKit.ForegroundAction;
-
 import org.junit.Test;
-
 
 public class GoGameTest {
    int nbLibertiesByDefault = 4;
-   
+
    // a stone can
    @Test
    public void aStoneCanBeTakenIfItHasIOneLiberty() {
@@ -25,11 +21,11 @@ public class GoGameTest {
 
    public boolean canBeTaken(int nbCornersTaken) {
       int nbLiberties = liberties(nbCornersTaken);
-      return (nbLiberties==1);
+      return (nbLiberties == 1);
    }
 
    private int liberties(int nbCornersTaken) {
-      int nbLiberties = nbLibertiesByDefault-nbCornersTaken;
+      int nbLiberties = nbLibertiesByDefault - nbCornersTaken;
       return nbLiberties;
    }
 
@@ -38,44 +34,64 @@ public class GoGameTest {
       int nbCornersTaken = 2;
       assertThat(canBeTaken(nbCornersTaken), equalTo(false));
    }
-   
-   @Test 
+
+   @Test
    public void theNumberLibertiesOfAGroupOfTwoIsTheSumOfAllStonesLiberties() {
-      List<String> fieldsTaken = list("b2", "c2");
-      List<String> cornersForB2 = list("a2", "b1", "c2", "b3");
-      List<String> cornersForC2 = list("b2", "c1", "d2", "c3" );
-      Set<String> corners = new TreeSet<String>(cornersForB2);
-      corners.addAll(cornersForC2);
-      corners.removeAll(fieldsTaken);
-      int totalLiberties = corners.size();
+      List<String> fieldsTaken = asList("22", "32");
+      List<String> cornersForB2 = neighboringFields("22");
+      List<String> cornersForC2 = neighboringFields("32");
+      int totalLiberties = cornersFreeForTwoFields(fieldsTaken, cornersForB2, cornersForC2);
       assertThat(totalLiberties, equalTo(6));
    }
-   
-   @Test 
+
+   @Test
    public void theNumberLibertiesOfAGroupOfThreeIsTheSumOfAllStonesLiberties() {
-      List<String> fieldsTaken = list("b2", "c2", "d2");
-      int cornersTakenForB2 = 1;
-      int cornersTakenForc2 = 2;
-      int cornersTakenFord2 = 1;
-      int totalLiberties = liberties(cornersTakenForB2) + liberties(cornersTakenForc2) + liberties(cornersTakenFord2) ;
+      List<String> fieldsTaken = asList("22", "32", "42");
+      List<String> cornersForB2 = neighboringFields("22");
+      List<String> cornersForC2 = neighboringFields("32");
+      List<String> cornersForD2 = neighboringFields("42");
+      int totalLiberties = cornersFreeForTwoFields(fieldsTaken, cornersForB2, cornersForC2, cornersForD2);
       assertThat(totalLiberties, equalTo(8));
    }
-   
-   private List<String> list(String...fields) {
-      List<String > list = new ArrayList<String>();
-      for (String field : fields) {
-         list.add(field);
+
+   /*
+    * 11 12 13 
+    * 21 22 23 
+    * 31 32 33
+    */
+   @Test
+   public void neighboringFieldsReturnsTheFourSurroundingFields() throws Exception {
+      assertThat(neighboringFields("22"), equalTo(asList("12", "23", "32", "21")));
+   }
+
+   private List<String> neighboringFields(String field) {
+      String row = field.substring(0, 1);
+      String column = field.substring(1);
+
+      String rowAbove = String.valueOf(Integer.valueOf(row) - 1);
+      String rowBelow = String.valueOf(Integer.valueOf(row) + 1);
+      String columnLeft = String.valueOf(Integer.valueOf(column) - 1);
+      String columnRight = String.valueOf(Integer.valueOf(column) + 1);
+
+      return asList(rowAbove.concat(column), row.concat(columnRight), rowBelow.concat(column), row.concat(columnLeft));
+   }
+
+   private int cornersFreeForTwoFields(List<String> fieldsTaken, List<String>... cornersForEveryElement) {
+      Set<String> corners = new TreeSet<String>();
+      for (List<String> cornersForX : cornersForEveryElement) {
+         corners.addAll(cornersForX);
       }
-      return list;
+      corners.removeAll(fieldsTaken);
+      return corners.size();
    }
 
    @Test
    public void aGroupCanBeTakenIfItHasOnlyOneLIberty() {
-      
+
    }
-   
-   //theGameISOverWhenAPlayerCapturesAtleastOneStone
+
+   // theGameISOverWhenAPlayerCapturesAtleastOneStone
    // aPlayerCanPlayOnAFieldThatISntTakenAlready
-   ///...
-   
+   // /...
+
 }
