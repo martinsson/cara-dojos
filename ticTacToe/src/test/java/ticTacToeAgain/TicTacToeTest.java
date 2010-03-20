ddcad3fac5fc25fbf4c9d8669c5746067fc100d6
unknown <Johan@.(none)>
2010-03-20 23:55:20 +0100

    start of ticTacToeSecondTake

---
+++ ticTacToe/src/test/java/ticTacToeAgain/TicTacToeTest.java
@@ -0,0 +1,13 @@
+package ticTacToeAgain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicTacToeTest {
+
+	@Test
+  public void testname() throws Exception {
+	  
+  }
+}
