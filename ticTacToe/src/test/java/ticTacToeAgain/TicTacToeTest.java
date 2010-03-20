c63dca8e1ae2208164d4cb4fe903356957ffadc3
unknown <Johan@.(none)>
2010-03-20 23:56:14 +0100

    now that's the real start

--- ticTacToe/src/test/java/ticTacToeAgain/TicTacToeTest.java
+++ ticTacToe/src/test/java/ticTacToeAgain/TicTacToeTest.java
@@ -5,6 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TicTacToeTest {
+	/*
+	a game is over when all fields are taken
+	a game is over when all fields in a column are taken by a player
+	a game is over when all fields in a row are taken by a player
+	a game is over when all fields in a diagonal are taken by a player
+	a player can take a field if not already taken
+	players take turns taking fields until the game is over
+	 */
 
 	@Test
   public void testname() throws Exception {
