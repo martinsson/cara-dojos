f83d0aa2c350180f97082d8a21ed6018ff4d2d61
Johan Martinsson <jm1974@hotmail.com>
2011-01-29 23:53:18 +0100

    extracted ReportPublisher using only automated refactorings

--- dojos/src/main/java/telldontask/KeywordReporter.java
+++ dojos/src/main/java/telldontask/KeywordReporter.java
@@ -1,20 +1,16 @@
 package telldontask;
 
-import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import telldontask.Filter.NoFilter;
 
 public class KeywordReporter {
 
-    private static final String REPORT_FILE = "report.csv";
     private final KeywordProvider keywordProvider;
     private final Filter filter;
-
+    private final ReportPublisher publisher = new ReportPublisher();
+    
     public KeywordReporter(KeywordProvider keywordProvider) {
         this(keywordProvider, new NoFilter());
     }
@@ -27,24 +23,7 @@ public class KeywordReporter {
     public void report() throws IOException {
         List<Keyword> keywords = keywordProvider.getKeywords();
         List<Keyword> filteredKeywords = filter.filter(keywords);
-        printReport(filteredKeywords);
-    }
-
-    private void printReport(List<Keyword> filteredKeywords) throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(REPORT_FILE)));
-        writeHeader(writer);
-        writeLines(writer, filteredKeywords);
-        writer.close();
-    }
-
-    private void writeLines(PrintWriter writer, List<Keyword> filteredKeywords) {
-        for (Keyword keyword : filteredKeywords) {
-            writer.println(keyword.getSearchTerm() +"; " + keyword.isPublished());
-        }
-    }
-
-    private void writeHeader(PrintWriter writer) {
-        writer.println("Search term; published");
+        publisher.printReport(filteredKeywords);
     }
 
 }
