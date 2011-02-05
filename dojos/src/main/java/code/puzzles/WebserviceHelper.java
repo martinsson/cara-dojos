5c44cc26defc29dcadb7620003088a4c47abab90
Johan Martinsson <jm1974@hotmail.com>
2011-02-05 12:29:31 +0100

    a solution with abstract class

--- dojos/src/main/java/code/puzzles/WebserviceHelper.java
+++ dojos/src/main/java/code/puzzles/WebserviceHelper.java
@@ -3,29 +3,53 @@ package code.puzzles;
 public class WebserviceHelper {
     private final Webservice webservice = new Webservice();
     public Products getProducts(final Integer category, final Integer quantity) throws WebserviceException{
-        Products products;
-        try {
-            products = webservice.getProducts(category, quantity);
-        } catch(RuntimeException e) {
-            throw new WebserviceException("failed invoking the webservice with category "+category+" and quantity "+quantity, e);
-        }
-        ifNullThrowError(products);
-        return products;
+        WebserviceInvoker<Products> invoker = new WebserviceInvoker<Products>() {
+            public Products invoke() {
+                return webservice.getProducts(category, quantity);
+            }
+
+            public String parameters() {
+                return "category "+category+" and quantity "+quantity;
+            }
+        };
+        return invoker.invokeSafely();
     }
     public Rebates getRebates(final Products products) throws WebserviceException{
-        Rebates rebates;
-        try {
-            rebates = webservice.getRebates(products);
-        } catch(RuntimeException e) {
-            throw new WebserviceException("failed invoking the webservice with products "+products);
+        WebserviceInvoker<Rebates> invoker = new WebserviceInvoker<Rebates>() {
+
+            @Override
+            protected Rebates invoke() {
+                return webservice.getRebates(products);
+            }
+
+            @Override
+            protected String parameters() {
+                return "products "+products;
+            }
+        };
+        return invoker.invokeSafely();
+    }
+
+    static abstract class WebserviceInvoker<T> {
+        protected abstract T invoke();
+        protected abstract String parameters();
+        public T invokeSafely() throws WebserviceException {
+            T t;
+            try {
+                t = invoke();
+            } catch(RuntimeException e) {
+                throw new WebserviceException("failed invoking the webservice with " + parameters());
+            }
+            ifNullThrowError(t);
+            return t;
+            
         }
-        ifNullThrowError(rebates);
-        return rebates;
+        private void ifNullThrowError(T t) throws WebserviceException {
+            if (t==null)
+                throw new WebserviceException("The webservice returned null");
+         }
     }
-    private void ifNullThrowError(Object object) throws WebserviceException {
-       if (object==null)
-           throw new WebserviceException("The webservice returned null");
-    }
+
     
     static class Webservice {
         public Products getProducts(Integer category, Integer quantity) {
