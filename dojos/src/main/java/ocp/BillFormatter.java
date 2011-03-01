c95c2bf79c9bd2a929c956f8cb346cf1f52c7ffa
Johan Martinsson <jm1974@hotmail.com>
2011-03-01 23:02:30 +0100

    added exception that eventually (after refactoring) wont be necessary

--- dojos/src/main/java/ocp/BillFormatter.java
+++ dojos/src/main/java/ocp/BillFormatter.java
@@ -2,12 +2,14 @@ package ocp;
 
 public class BillFormatter {
 
-    public String format(int price, Currency currency) {
+    public String format(int price, Currency currency) throws UnknownCurrencyException {
         String formattedPrice = "Your total is : ";
         if (currency == Currency.EUROS) {
             formattedPrice += price + "Û";
         } else if (currency == Currency.POUNDS) {
             formattedPrice += "£" + price;
+        } else {
+            throw new UnknownCurrencyException();
         }
         return formattedPrice;
     }
